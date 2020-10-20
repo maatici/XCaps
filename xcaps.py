2219f381489d733e1ae95e7a0d1ffce55757bb45
Mehmet Ali ATICI <37369384+maatici@users.noreply.github.com>
2020-10-20 11:43:00 +0300

    Create xcaps.py

---
+++ xcaps.py
@@ -0,0 +1,123 @@
+#!pip install q keras == 2.1.2
+from __future__ import print_function
+from keras import backend as K
+
+from keras import activations
+
+from keras.layers import *
+from keras import Model
+from keras.applications.xception import Xception
+
+
+#Capsule implementation taken from  https://github.com/bojone/Capsule/ in 2019
+
+def squash(x, axis=-1):
+    s_squared_norm = K.sum(K.square(x), axis, keepdims=True) + K.epsilon()
+    scale = K.sqrt(s_squared_norm) / (1 + s_squared_norm)
+    return scale * x
+
+def softmax(x, axis=-1):
+    ex = K.exp(x - K.max(x, axis=axis, keepdims=True))
+    return ex / K.sum(ex, axis=axis, keepdims=True)
+
+def margin_loss(y_true, y_pred):
+    lamb, margin = 0.5, 0.1
+    return K.sum(y_true * K.square(K.relu(1 - margin - y_pred)) + lamb * (
+            1 - y_true) * K.square(K.relu(y_pred - margin)), axis=-1)
+
+class Capsule(Layer):
+    """A Capsule Implement with Pure Keras
+    There are two vesions of Capsule.
+    One is like dense layer (for the fixed-shape input),
+    and the other is like timedistributed dense (for various length input).
+
+    The input shape of Capsule must be (batch_size,
+                                        input_num_capsule,
+                                        input_dim_capsule
+                                       )
+    and the output shape is (batch_size,
+                             num_capsule,
+                             dim_capsule
+                            )
+
+    Capsule Implement is from https://github.com/bojone/Capsule/
+    Capsule Paper: https://arxiv.org/abs/1710.09829
+    """
+
+    def __init__(self,
+                 num_capsule,
+                 dim_capsule,
+                 routings=3,
+                 share_weights=True,
+                 activation='squash',
+                 kval = 0,
+                 **kwargs):
+        super(Capsule, self).__init__(**kwargs)
+        self.num_capsule = num_capsule
+        self.dim_capsule = dim_capsule
+        self.routings = routings
+        self.share_weights = share_weights
+        self.kval = kval
+        if activation == 'squash':
+            self.activation = squash
+        else:
+            self.activation = activations.get(activation)
+
+    def build(self, input_shape):
+        input_dim_capsule = input_shape[-1]
+        if self.share_weights:
+            self.kernel = self.add_weight(
+                name='capsule_kernel',
+                shape=(1, input_dim_capsule,
+                       self.num_capsule * self.dim_capsule),
+                initializer='random_uniform',
+                trainable=True)
+        else:
+            input_num_capsule = input_shape[-2]
+            self.kernel = self.add_weight(
+                name='capsule_kernel',
+                shape=(input_num_capsule, input_dim_capsule,
+                       self.num_capsule * self.dim_capsule),
+                initializer='random_uniform',  # 'glorot_uniform',
+                trainable=True)
+
+    def call(self, inputs):
+        if self.share_weights:
+            hat_inputs = K.conv1d(inputs, self.kernel)
+        else:
+            hat_inputs = K.local_conv1d(inputs, self.kernel, [1], [1])
+
+        batch_size = K.shape(inputs)[0]
+        input_num_capsule = K.shape(inputs)[1]
+        hat_inputs = K.reshape(hat_inputs,
+                               (batch_size, input_num_capsule,
+                                self.num_capsule, self.dim_capsule))
+        hat_inputs = K.permute_dimensions(hat_inputs, (0, 2, 1, 3))
+
+        b = K.zeros_like(hat_inputs[:, :, :, 0])
+        for i in range(self.routings):
+            c = softmax(b, 1)
+            o = self.activation(K.batch_dot(c, hat_inputs, [2, 2]))
+            if i < self.routings - 1:
+                b = b*self.kval + K.batch_dot(o, hat_inputs, [2, 3])
+                if K.backend() == 'theano':
+                    o = K.sum(o, axis=1)
+
+        return o
+
+    def compute_output_shape(self, input_shape):
+        return (None, self.num_capsule, self.dim_capsule)
+
+# XCaps implementation
+
+    def getXCaps(self):
+        model = Xception(weights=None, include_top=False, input_shape=(128,128,1))
+        next_layer = model.output
+        next_layer = GlobalAveragePooling2D()(next_layer)
+        next_layer = Reshape((-1, 128))(next_layer)
+        capsule = Capsule(3, 16, 3, True,kval=1)(next_layer)
+        output = Lambda(lambda z: K.sqrt(K.sum(K.square(z), 2)))(capsule)
+        xcapsmodel = Model(inputs=model.input, outputs=output)
+        xcapsmodel.compile(loss=margin_loss, optimizer='adam', metrics=['accuracy'])  # optimizer='sgd'
+        print(xcapsmodel.summary())
+        return xcapsmodel
